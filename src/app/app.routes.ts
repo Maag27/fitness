2ddb585c3ae3ac3cadaf7bf6d504e63e5b87b743
NovaPage <novapagecompany@gmail.com>
2024-10-11 05:40:49 -0500

    se logro ocultar el footer y el navbar cuando se esta en login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,8 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }, // Añade la ruta para HomeComponent
-
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  // otras rutas
+  { path: 'login', component: LoginComponent, data: { hideNavbarFooter: true } },
+  { path: 'register', component: RegisterComponent, data: { hideNavbarFooter: true } },
+  { path: '', component: HomeComponent, data: { hideNavbarFooter: false } },
+    { path: 'home', component: HomeComponent }, // Añade la ruta para HomeComponent
 ];
