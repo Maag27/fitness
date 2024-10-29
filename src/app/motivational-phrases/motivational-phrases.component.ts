01fb7635514fce4f2d7b4d9f44757eff1ee0f180
NovaPage <novapagecompany@gmail.com>
2024-10-29 18:44:12 -0500

    Se configuro correctamente las frases motivacionales de manera aleatoria

--- src/app/motivational-phrases/motivational-phrases.component.ts
+++ src/app/motivational-phrases/motivational-phrases.component.ts
@@ -10,22 +10,29 @@ import { PhrasesService } from '../services/phrases.service';
   providers: [PhrasesService]
 })
 export class MotivationalPhrasesComponent implements OnInit {
-  phrases: any[] = [];
-  currentPhrase: string = '';
+  phrases: any[] = []; // Arreglo para almacenar las frases y autores obtenidos de Firestore
+  currentPhrase: string = ''; // Frase actual mostrada en el componente
+  currentAuthor: string = ''; // Autor actual mostrado en el componente
 
   constructor(private phrasesService: PhrasesService) {}
 
   ngOnInit(): void {
+    // Obtiene las frases de Firestore al inicializar el componente
     this.phrasesService.getPhrases().subscribe(data => {
-      this.phrases = data;
+      this.phrases = data; // Almacena las frases en el arreglo
       this.selectRandomPhrase(); // Selecciona una frase inicial
     });
   }
 
   selectRandomPhrase(): void {
+    // Selecciona una frase aleatoria de la lista de frases disponibles
     if (this.phrases.length > 0) {
       const randomIndex = Math.floor(Math.random() * this.phrases.length);
-      this.currentPhrase = this.phrases[randomIndex].text;
+      const randomPhrase = this.phrases[randomIndex];
+      
+      // Asigna la frase y el autor a las variables actuales
+      this.currentPhrase = randomPhrase.frase; 
+      this.currentAuthor = randomPhrase.Autor;
     }
   }
 }
